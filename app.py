806c780f71473066b079686e47d361978100610d
SdMusic <peter_freemantle@hotmail.com>
2021-04-07 09:22:01 +0000

    Add Register template

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import (
     Flask, flash, render_template,
-     redirect, request, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,8 +25,12 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)  
-             
+            debug=True)
